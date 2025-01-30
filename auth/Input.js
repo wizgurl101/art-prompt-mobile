68d9ac139e4abef3bf6b93d5dd6941ea0da9359b
wizgurl101 <nb.phan@hotmail.com>
2025-01-30 14:58:55 -0700

    add components

--- auth/Input.js
+++ auth/Input.js
@@ -18,7 +18,6 @@ function Input({
       <TextInput
         style={[styles.input, isInvalid && styles.inputInvalid]}
         autoCapitalize={false}
-        autoCapitalize="none"
         keyboardType={keyboardType}
         secureTextEntry={secure}
         onChangeText={onUpdateValue}
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,4 +1,4 @@
-import AuthContent from "../components/Auth/AuthContent";
+import AuthContent from "../auth/AuthContent";
 
 function LoginScreen() {
   return <AuthContent isLogin />;
