a8f8ad9510841e57cf7f86b1f5c9892ca7a0e99a
wizgurl101 <nb.phan@hotmail.com>
2025-02-01 22:00:48 -0700

    add andriod and ios folder

--- screens/Login/auth/AuthContent.js
+++ screens/Login/auth/AuthContent.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Alert, StyleSheet, View, Text } from "react-native";
+import Rive from "rive-react-native";
 
 import LoginForm from "./LoginForm";
 import { Colors } from "../../../constants/styles";
@@ -37,7 +38,12 @@ function AuthContent({ isLogin, onAuthenticate }) {
   return (
     <>
       <View style={styles.title}>
-        <Text style={styles.titleText}>Art Prompt</Text>
+        <Rive
+          url="https://public.rive.app/community/runtime-files/2195-4346-avatar-pack-use-case.riv"
+          artboardName="Avatar 1"
+          stateMachineName="avatar"
+          style={{ width: 400, height: 400 }}
+        />
       </View>
       <View style={styles.login}>
         <LoginForm
